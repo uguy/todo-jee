98e9d6dea8ec55051c47a16b037ae8e6d16e410b
Uguy <hugues.bretin@gmail.com>
2014-02-06 16:31:08 +0100

    added bootstrap makeup

--- todo-ejb/src/main/java/com/od/jee/sample/model/Task.java
+++ todo-ejb/src/main/java/com/od/jee/sample/model/Task.java
@@ -2,15 +2,12 @@ package com.od.jee.sample.model;
 
 import java.io.Serializable;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
-import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlRootElement;
 
@@ -27,12 +24,13 @@ public class Task implements Serializable {
 
 	@NotNull
 	@Size(min = 1, max = 25)
-	@Pattern(regexp = "[A-Za-z ]*", message = "must contain only letters and spaces")
 	private String title;
 
 	@NotNull
 	@Size(min = 1, max = 250)
 	private String description;
+
+	private boolean done = false;
 
 	public Long getId() {
 		return id;
@@ -58,4 +56,11 @@ public class Task implements Serializable {
 		this.description = description;
 	}
 
+	public boolean isDone() {
+		return done;
+	}
+
+	public void setDone(boolean done) {
+		this.done = done;
+	}
 }
